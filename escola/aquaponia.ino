05fff60af8b3469253c640b6c77f234381233f42
LuisHenriqueDaSilv <luishenriquen14@gmail.com>
2023-06-20 22:29:33 -0300

    .

--- escola/aquaponia.ino
+++ escola/aquaponia.ino
@@ -5,11 +5,16 @@
 #include <Wire.h>  
 
 
-#define    A0
+#define PORTA_SENSOR_DE_AGUA A0
 #define PORTA_SENSOR_DE_TEMPERATURA 7
+#define PORTA_RELE 6 
+#define PORTA_LED_VERMELHO 5
+#define PORTA_LED_VERDE 4 
+#define PORTA_LED_AZUL 3
 
-int valorNivelDeAgua = 0;
 int leituraTemperatura = 0;
+volatile int ultimo_momento_pressionado = 0;
+bool bombaEstaLigada = false;
 
 LiquidCrystal_I2C displayLCD(0x27,20,4);
 OneWire oneWire(PORTA_SENSOR_DE_TEMPERATURA);
@@ -41,28 +46,44 @@ escreverLCD(String primeiraLinha, String segundaLinha = ""){
 
 void setup() {
   Serial.begin(9600);
-  
+
+  pinMode(PORTA_RELE, OUTPUT);
+  pinMode(PORTA_LED_AZUL, OUTPUT);
+  pinMode(PORTA_LED_VERDE, OUTPUT);
+  pinMode(PORTA_LED_VERMELHO, OUTPUT);
+
   SensorDeTemperatura.begin();
   
   displayLCD.init();
   displayLCD.backlight();
   displayLCD.clear();
+
+  digitalWrite(PORTA_RELE, HIGH)
+
+
 }
 
 void loop() {
   delay(10);
   SensorDeTemperatura.requestTemperatures();
   
-  valorNivelDeAgua = analogRead(PORTA_SENSOR_DE_AGUA)/10; 
   leituraTemperatura=SensorDeTemperatura.getTempCByIndex(0);
+
+  digitalWrite(PORTA_LED_AZUL, LOW);
+  digitalWrite(PORTA_LED_VERDE, LOW);
+  digitalWrite(PORTA_LED_VERMELHO, LOW);
   
-  Serial.println(valorNivelDeAgua);
-  Serial.println(leituraTemperatura);
-  // escreverLCD(String("Nivel agua:"), String("Temp da agua:") );
-  escreverLCD(String("Nivel agua:") + String(valorNivelDeAgua), String("Temp da agua:") + String(leituraTemperatura) + String("c") );
+  if(leituraTemperatura > 30){
+    escreverLCD(String("Temp da agua:") + String(leituraTemperatura) + String("c"), String("Quente"));
+    digitalWrite(PORTA_LED_VERMELHO, HIGH);
+  } else if (leituraTemperatura < 25){
+    escreverLCD(String("Temp da agua:") + String(leituraTemperatura) + String("c"), String("Frio"));
+    digitalWrite(PORTA_LED_AZUL, HIGH);
+  } else {
+    escreverLCD(String("Temp da agua:") + String(leituraTemperatura) + String("c"), String("Ideal"));
+    digitalWrite(PORTA_LED_VERDE, HIGH);
+  }
 
-
-
-  delay(200);
+  delay(1000);
 }
 
